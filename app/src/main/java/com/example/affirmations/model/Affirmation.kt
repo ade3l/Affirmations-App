ff01bc5a522b71171228c09981a00043ebd1ea90
Adeel <71775487+ade3l@users.noreply.github.com>
2021-10-29 16:21:58 +0300

    reorganised code

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -3,6 +3,4 @@ package com.example.affirmations.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class Affirmation(
-    @StringRes val stringResourceId:Int, @DrawableRes val imageResourceId:Int
-    )
+data class Affirmation(@StringRes val stringResourceId:Int, @DrawableRes val imageResourceId:Int)
